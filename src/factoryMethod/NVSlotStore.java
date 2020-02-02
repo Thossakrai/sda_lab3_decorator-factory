8a185bf7cea4f904eeea9e3b0c0472fd9ef6966b
dewkul <jirapat0811@gmail.com>
2020-02-02 22:21:13 +0700

    Add more type

--- src/factoryMethod/NVSlotStore.java
+++ src/factoryMethod/NVSlotStore.java
@@ -4,6 +4,8 @@ public class NVSlotStore extends SlotStore {
 	
 	protected Slot makeSlot(slottype type) {
 		if (type.equals(slottype.progressive)) return new NVSlotProgressive();
+		else if (type.equals(slottype.bonus)) return new NVSlotBonus();
+		else if (type.equals(slottype.straight)) return new NVSlotStraight();
 		else return null;
 	}
 }
